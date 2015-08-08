a0bc51c2ba44d61d4dc2ee6e470cc64b6cf0f874
noderosa <chie.miyajima@gmail.com>
2015-08-07 21:52:44 -0400

    updates

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+
+		var questions = [{
+		q: "All that we are is the result of what we have thought.",
+		c: ["John Lennon", "Mahatma Gandhi", "Buddha"], 
+		a: "Buddha",
+		},
+		{
+		q: "Quality is more important than quantity. One home run is much better than two doubles.",
+		c: ["Steve Jobs", "Joe Torre", "Joe Biden"],
+		a: "Steve Jobs",
+		}
+		{
+		q: "Why is it that nobody understands me, yet everybody likes me?",
+		c: ["Donald Trump", "Albert Einstein", "Kanye West"],
+		a: "Albert Einstein",
+		}
+		{
+		q: "Everything has beauty, but not everyone sees it.",
+		c: ["Winnie the Pooh", "Kim Kardashian", "Confucious"],
+		a: "Confucious",
+		}
+		{
+		q: "True friends stab you in the front.",
+		c: ["Oscar Wilde", "Nas", "Socrates"],
+		a: "Oscar Wilde",
+		}];
+	
+	
+		function nextQuestion(){
+
+			
+	}
+
+
+	$(".submit").on('click', function (enter);{
+		event.preventDefault(); 
+		nextQuestion();
+		
+	}
+
+	
+
+
+)}
+
